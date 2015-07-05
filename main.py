c587f996705183ffc847ac435655fab946bb5f0d
skylarker <arunprasath9586@gmail.com>
2015-07-04 17:43:21 -0700

    laying out

---
+++ main.py
@@ -0,0 +1,6 @@
+class Main(object):
+    def __init__(self):
+        pass
+
+if '__name__' == '__main__':
+    main = Main()
---
+++ utils/list/sortby.py
@@ -0,0 +1,7 @@
+class SortBy(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def sort(list_):
+        return sorted(list_)
